52b329e5c1e72e3508430617374c1eb0c05c5d33
samarjitsutradhar36246-dev <samarjitsutradhar36246@gmail.com>
2025-11-06 22:22:52 +0530

    Added Map method and did conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  const [count, setCount] = useState(0)
-
+  let foodItems = ["Salad", "Paneer", "Dal", "Fruits", "Curd"];
+  // let foodItems = [];
+  let emptyMessege = foodItems.length === 0 ? <h3>I'm still hungry</h3> : null;
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Healthy Foods</h1>
+      {emptyMessege}
+      <ul className="list-group">
+        {foodItems.map((items) => (
+          <li key={items} className="list-group-item">
+            {items}
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
